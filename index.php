692d516af96e0846f07814772ae10318294ab81c
masaokaka <osushiiisensu@gmail.com>
2021-12-22 17:36:42 +0900

    update: DBのpsotテーブルのデータをすべて取得してwhileで表示

--- index.php
+++ index.php
@@ -14,24 +14,31 @@ include("includes/navigation.php")
     <div class="row">
         <!-- Blog Entries Column -->
         <div class="col-md-8">
-            <h1 class="page-header">
-                Page Heading
-                <small>Secondary Text</small>
-            </h1>
-            <!-- First Blog Post -->
-            <h2>
-                <a href="#">Blog Post Title</a>
-            </h2>
-            <p class="lead">
-                by <a href="index.php">Start Bootstrap</a>
-            </p>
-            <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
-            <hr>
-            <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-            <hr>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
-            <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-            <hr>
+            <!-- Blog Post -->
+            <?php
+            $query = "SELECT * FROM posts";
+            $result = mysqli_query($connection, $query);
+            while ($row = mysqli_fetch_assoc($result)) {
+            ?>
+                <h1 class="page-header">
+                    Page Heading
+                    <small>Secondary Text</small>
+                </h1>
+                <h2>
+                    <a href="index.php"><?php echo $row["post_title"] ?></a>
+                </h2>
+                <p class="lead">
+                    by <a href="index.php"><?php echo $row["post_author"] ?></a>
+                </p>
+                <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $row["post_date"] ?></p>
+                <hr>
+                <img class="img-responsive" src=<?php echo $row["post_image"] ?> alt="img_posted">
+                <hr>
+                <p><?php echo $row["post_content"] ?></p>
+                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                <hr>
+            <?php
+            } ?>
         </div>
 
         <!-- Blog Sidebar Widgets Column -->
